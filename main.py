772f97e76f7a8afe1e79cdee101f3770b3a9e5fb
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2020-11-30 18:13:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,19 +20,28 @@ flags.DEFINE_integer("batch_size", 2, "batch size")
 
 flags.DEFINE_integer("output_size", 7, "")
 
+flags.DEFINE_float("coord_lambda", 5.0, "")
+
+flags.DEFINE_float("noObject_lambda", 0.5, "")
+
+flags.DEFINE_float("lr", 0.001, "Leanring rate")
+
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
+
+optim = tf.keras.optimizers.SGD(FLAGS.lr, momentum=0.9)
 
 def func_(image, label):
 
     image = tf.io.read_file(image)
     image = tf.image.decode_jpeg(image, 3)
+    original_height, original_width = image.shape[0], image.shape[1]
     image = tf.image.resize(image, [FLAGS.img_size, FLAGS.img_size])
 
     image = tf.image.convert_image_dtype(image, tf.float32) / 255.
     #image = tf.image.per_image_standardization(image)
 
-    return image, label
+    return image, label, original_height, original_width
 
 def read_label(file, batch_size):
     # https://github.com/lovish1234/YOLOv1/blob/master/preprocess.py
@@ -72,8 +81,8 @@ def read_label(file, batch_size):
             offset_x = ((C_x % cell_w) * 1.0) / cell_w
             offset_y = ((C_y % cell_h) * 1.0) / cell_h
 
-            offset_w = math.sqrt(b_w * 1.0) / width
-            offset_h = math.sqrt(b_h * 1.0) / height
+            offset_w = math.sqrt(b_w * 1.0 / width)
+            offset_h = math.sqrt(b_h * 1.0 / height)
 
             boxData = [offset_x, offset_y, offset_w, offset_h]
 
@@ -89,6 +98,72 @@ def read_label(file, batch_size):
     label = np.array(label, dtype=np.float32)
 
     return label
+
+def cal_iou(predict_box1, predict_box2, label_box):
+    def get_iou(label_box, predict_box):
+
+        intersection_width = tf.minimum(predict_box[:, :, :, 0] + 0.5*predict_box[:, :, :, 2],
+                                        label_box[:, :, :, 0] + 0.5*label_box[:, :, :, 2]) \
+                            - tf.maximum(predict_box[:, :, :, 0] - 0.5*predict_box[:, :, :, 2],
+                                            label_box[:, :, :, 0] - 0.5*label_box[:, :, :, 2])
+
+        intersection_height = tf.minimum(predict_box[:, :, :, 1] + 0.5*predict_box[:, :, :, 3],
+                                        label_box[:, :, :, 1] + 0.5*label_box[:, :, :, 3]) \
+                            - tf.maximum(predict_box[:, :, :, 1] - 0.5*predict_box[:, :, :, 3],
+                                            label_box[:, :, :, 1] - 0.5*label_box[:, :, :, 3])
+
+        intersection = tf.multiply(tf.maximum(0, intersection_height), tf.maximum(0, intersection_width))
+
+        union = tf.subtract(tf.multiply(predict_box[:, :, :, 2], predict_box[:, :, :, 3]) + tf.multiply(label_box[:, :, :, 2], label_box[:, :, :, 3]),intersection)
+
+        iou = tf.divide(intersection, union)
+
+        return iou
+
+    iou1 = tf.reshape(get_iou(label_box, predict_box1), [-1, 7, 7, 1])
+    iou2 = tf.reshape(get_iou(label_box, predict_box2), [-1, 7, 7, 1])
+    return tf.concat([iou1, iou2], 3)
+
+def save_fig(model, images, original_height, original_width):
+
+    output = model(images, False)
+
+    predictedObjects = []
+
+    pred_class = (output[:,:,:,:20])
+    predictedObjectConfidence = output[:, :, :, 20:22]
+    class_confidences = tf.split(pred_class, num_or_size_splits=20, axis=-1)
+
+    predictedBoxes = output[:, :, :, 22:]
+    predictedFirstBoxes = predictedBoxes[:, :, :, :4]
+    predictedFirstBoxes = tf.reshape(predictedFirstBoxes, [-1, 7, 7, 1, 4])
+    predictedSecondBoxes = predictedBoxes[:, :, :, 4:]
+    predictedSecondBoxes = tf.reshape(predictedSecondBoxes, [-1, 7, 7, 1, 4])
+    result = tf.concat([predictedFirstBoxes, predictedSecondBoxes], 3)
+    np_result = result.numpy()
+    np_result = np.array(np_result)
+
+    offset_Y = np.tile(np.arange(7)[:, np.newaxis, np.newaxis],(1, 7, 2))
+    offset_X = np.transpose(offset_Y, (1, 0, 2))
+
+    for i in range(FLAGS.batch_size):
+        np_result_ = np_result[i]
+
+        np_result_[:, :, :, 0] = 1. * (np_result_[:, :, :, 0]+offset_X) * original_width
+        np_result_[:, :, :, 1] = 1. * (np_result_[:, :, :, 1]+offset_Y) * original_height
+
+        np_result_[:, :, :, 2] = original_width * np.multiply(np_result_[:, :, :, 2],
+                                                              np_result_[:, :, :, 2])
+        np_result_[:, :, :, 3] = original_width * np.multiply(np_result_[:, :, :, 3],
+                                                              np_result_[:, :, :, 3])
+
+        for class_confi in class_confidences:
+            scores = predictedObjectConfidence * class_confi
+            scores = tf.reshape(scores, [-1])
+
+        # [7,7,2,20]
+
+    return img
 
 def cal_loss(model, images, labels):
     # https://github.com/lovish1234/YOLOv1/blob/master/yolo.py
@@ -106,21 +181,46 @@ def cal_loss(model, images, labels):
     24-72 Cell has an object/no object (Only one can be is 1)
     """
 
-    with tf.GradientTape() as tape:
+    with tf.GradientTape(persistent=True) as tape:
         logits = model(images, training=True)
-        pred_class = logits[:,:,:,:20]
+        pred_class = (logits[:,:,:,:20])
         predictedObjectConfidence = logits[:, :, :, 20:22]
         predictedBoxes = logits[:, :, :, 22:]
         predictedFirstBoxes = predictedBoxes[:, :, :, :4]
-        predictedSecondBoxes = predictedBoxes[:, :, :, 5:]
+        predictedSecondBoxes = predictedBoxes[:, :, :, 4:]
 
         groundTruthClasses = labels[:, :, :, :20]
         groundTruthBoxes = labels[:, :, :, 20:24]
         groundTruthGrid = labels[:, :, :, 24:]
 
-        # 891 줄 부터 다음주 월요일에 다시하자!!!
+        firstBox_loss = tf.reduce_sum(tf.square(predictedFirstBoxes - groundTruthBoxes), 3)
+        secondBox_loss = tf.reduce_sum(tf.square(predictedSecondBoxes - groundTruthBoxes), 3)
+                
+        IOU = cal_iou(predictedFirstBoxes, predictedSecondBoxes, groundTruthBoxes)
+        responsibleBox = tf.greater(IOU[:, :, :, 0], IOU[:, :, :, 1])
+        coordinateLoss = tf.where(responsibleBox, firstBox_loss, secondBox_loss)    # [-1, 7, 7]
+        coordinateLoss = tf.reshape(coordinateLoss, [-1, 7, 7, 1])  # [-1, 7, 7, 1]
 
+        coordinateLoss = FLAGS.coord_lambda * tf.multiply(groundTruthGrid, coordinateLoss)
 
+        object_loss = tf.square(predictedObjectConfidence - groundTruthGrid)
+        object_loss = tf.where(responsibleBox, object_loss[:, :, :, 0], object_loss[:, :, :, 1])
+        object_loss = tf.reshape(object_loss, [-1, 7, 7, 1])
+
+        noObject_loss = FLAGS.noObject_lambda * tf.multiply(1 - groundTruthGrid, object_loss)
+        object_loss = tf.multiply(groundTruthGrid, object_loss)
+
+        class_loss = tf.square(pred_class - groundTruthClasses)
+        class_loss = tf.reduce_sum(tf.multiply(class_loss, groundTruthGrid), 3)
+        class_loss = tf.reshape(class_loss, [-1, 7, 7, 1])
+
+        total_loss = coordinateLoss + object_loss + noObject_loss + class_loss
+        total_loss = tf.reduce_mean(tf.reduce_sum(total_loss, [1,2,3]), 0)
+
+        # coordinateLoss need reshape??
+
+    grads = tape.gradient(total_loss, model.trainable_variables)
+    optim.apply_gradients(zip(grads, model.trainable_variables))
     return total_loss
 
 def main():
@@ -150,10 +250,14 @@ def main():
     batch_idx = len(text_list) // FLAGS.batch_size
     it = iter(data)
     for step in range(batch_idx):
-        image, label = next(it)   
+        image, label, original_height, original_width = next(it)   
         label = read_label(label, FLAGS.batch_size)
 
         loss = cal_loss(model, image, label)
+
+        save_fig(model, image, original_height, original_width)
+
+        print(loss)
 
 if __name__ == "__main__":
     main()
