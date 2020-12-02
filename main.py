524ee7c3cceca8cb25a484ac554cabca8d57da7a
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2020-12-02 20:47:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ flags.DEFINE_integer("batch_size", 16, "batch size")
 
 flags.DEFINE_integer("output_size", 7, "")
 
+flags.DEFINE_float("mini_threshold", 0.2, "")
+
 flags.DEFINE_float("coord_lambda", 5.0, "")
 
 flags.DEFINE_float("noObject_lambda", 0.5, "")
@@ -45,7 +47,7 @@ def func_(image, label):
     shape = tf.shape(image)
     image = tf.image.resize(image, [FLAGS.img_size, FLAGS.img_size])
 
-    image = tf.image.convert_image_dtype(image, tf.float32) / 255. * 2 - 1. # -1 ~ 1
+    image = tf.image.convert_image_dtype(image, tf.float32) / 255.  # -1 ~ 1
     #image = tf.image.per_image_standardization(image)
 
     return image, label, shape, list_
@@ -80,8 +82,10 @@ def read_label(file, batch_size):
 
             # take the center point and scale according to new size
 
-            C_x = ((xmin + xmax) * 1.0 / 2.0) * (FLAGS.img_size * 1.0 / width)
-            C_y = ((ymin + ymax) * 1.0 / 2.0) * (FLAGS.img_size * 1.0 / height)
+            C_x = ((xmin + xmax) * 1.0 / 2.0) * \
+                    (FLAGS.img_size * 1.0 / width)
+            C_y = ((ymin + ymax) * 1.0 / 2.0) * \
+                    (FLAGS.img_size * 1.0 / height)
             b_w = (xmax - xmin) * 1.0
             b_h = (ymax - ymin) * 1.0
 
@@ -96,9 +100,9 @@ def read_label(file, batch_size):
             responsibleGridX = int(C_x / cell_w)    # confidence coordinate
             responsibleGridY = int(C_y / cell_h)
 
-            responsibleGrid[b][responsibleGridX][responsibleGridY][classes] = 1    # class
-            responsibleGrid[b][responsibleGridX][responsibleGridY][20:24] = boxData    # box
-            responsibleGrid[b][responsibleGridX][responsibleGridY][24] = 1 # confidence
+            responsibleGrid[b][responsibleGridX][responsibleGridY][5:classes + 6] = 1    # class
+            responsibleGrid[b][responsibleGridX][responsibleGridY][0:4] = boxData    # box
+            responsibleGrid[b][responsibleGridX][responsibleGridY][4] = 1 # confidence
 
         #    cla.append(responsibleGrid)
 
@@ -146,44 +150,18 @@ def test_iou(box1, box2):
 
     return intersection / union
 
-def draw_bbox(img, bbox, text):
-    h, w = img.shape[0], img.shape[1]
-    x0, y0, x1, y1 = max(bbox[0], 0), max(bbox[1], 0), min(bbox[2], w-1), min(bbox[3], h-1)
-    drawed_img = img * 1
-    color = np.zeros([x1 - x0 + 1, 3])
-    color[:, 1] = np.ones([x1 - x0 + 1]) * 255#Green rectangle
-    drawed_img[y0, x0:x1 + 1, :] = color
-    drawed_img[y1, x0:x1 + 1, :] = color
-    color = np.zeros([y1 - y0 + 1, 3])
-    color[:, 1] = np.ones([y1 - y0 + 1]) * 255  # Green rectangle
-    drawed_img[y0:y1 + 1, x0, :] = color
-    drawed_img[y0:y1 + 1, x1, :] = color
-    #type text
-    drawed_img = Image.fromarray(np.uint8(drawed_img))
-    draw = ImageDraw.Draw(drawed_img)
-    x = int(bbox[0])
-    y = int(bbox[1])
-    draw.text((x, y), text)
-    drawed_img = np.array(drawed_img)
-    return drawed_img
-
 def save_fig(model, images, original_height, original_width):
 
     output = model(images, False)
 
-    pred_class = (output[:,:,:,:20])
-    pred_confidence = output[:, :, :, 20:22]
+    pred_class = tf.reshape(output[:, 0:980], [-1,7,7,20])
+    pred_confidence = tf.reshape(output[:, 980:1078], [-1, 7, 7, 2])
 
-    predictedBoxes = output[:, :, :, 22:]
-    predictedFirstBoxes = predictedBoxes[:, :, :, :4]
-    predictedFirstBoxes = tf.reshape(predictedFirstBoxes, [-1, 7, 7, 1, 4])
-    predictedSecondBoxes = predictedBoxes[:, :, :, 4:]
-    predictedSecondBoxes = tf.reshape(predictedSecondBoxes, [-1, 7, 7, 1, 4])
-    result = tf.concat([predictedFirstBoxes, predictedSecondBoxes], 3)
-    np_result = result.numpy()
+    predictedBoxes = tf.reshape(output[:, 1078:], [-1, 7, 7, 2, 4])
+    np_result = predictedBoxes.numpy()
     np_result = np.array(np_result)
 
-    offset_Y = np.tile(np.arange(7)[:, np.newaxis, np.newaxis],(1, 7, 2))
+    offset_Y = np.tile(np.arange(7, dtype=np.float32)[:, np.newaxis, np.newaxis],(1, 7, 2))
     offset_X = np.transpose(offset_Y, (1, 0, 2))
 
     for i in range(FLAGS.batch_size):
@@ -191,199 +169,123 @@ def save_fig(model, images, original_height, original_width):
         original_height_ = original_height[i].numpy()
         original_width_ = original_width[i].numpy()
 
-        image = tf.image.resize(images[i], [original_height_, original_width_])
-        image = image.numpy()
-
-        B = np.expand_dims(image[:, :, 2], 2)
-        G = np.expand_dims(image[:, :, 1], 2)
-        R = np.expand_dims(image[:, :, 0], 2)
-
-        image = np.concatenate([B,G,R], 2)
-
         np_result_ = np_result[i]
 
         np_result_[:, :, :, 0] = 1. * (np_result_[:, :, :, 0]+offset_X) * FLAGS.img_size / FLAGS.output_size
         np_result_[:, :, :, 1] = 1. * (np_result_[:, :, :, 1]+offset_Y) * FLAGS.img_size / FLAGS.output_size
 
-        np_result_[:, :, :, 2] = original_width_ * np.multiply(np_result_[:, :, :, 2],
+        np_result_[:, :, :, 2] = FLAGS.img_size * np.multiply(np_result_[:, :, :, 2],
                                                               np_result_[:, :, :, 2])
-        np_result_[:, :, :, 3] = original_height_ * np.multiply(np_result_[:, :, :, 3],
+        np_result_[:, :, :, 3] = FLAGS.img_size * np.multiply(np_result_[:, :, :, 3],
                                                               np_result_[:, :, :, 3])
 
-        #box_data = np_result_
-        x1 = np_result_[:, :, :, 0] - np_result_[:, :, :, 2]/2
-        y1 = np_result_[:, :, :, 1] - np_result_[:, :, :, 3]/2
-        x2 = np_result_[:, :, :, 0] + np_result_[:, :, :, 2]/2
-        y2 = np_result_[:, :, :, 1] + np_result_[:, :, :, 3]/2
+        np_result_[:, :, :, 0] = np_result_[:, :, :, 0] - np_result_[:, :, :, 2] / 2
+        np_result_[:, :, :, 1] = np_result_[:, :, :, 1] - np_result_[:, :, :, 3] / 2
+        np_result_[:, :, :, 2] = np_result_[:, :, :, 0] + np_result_[:, :, :, 2] / 2
+        np_result_[:, :, :, 3] = np_result_[:, :, :, 1] + np_result_[:, :, :, 3] / 2
 
-        box_data = tf.concat([x1, y1, x2, y2], -1)
-        box_data = tf.reshape(box_data, [7, 7, 2, 4])
-
-        pred_bboxes = tf.expand_dims(box_data, 0)   # [1, 7, 7, 2, 4]
-        pred_bboxes = tf.reshape(pred_bboxes, [-1, 4])
-        class_confidences = tf.split(pred_class[i], num_or_size_splits=20, axis=-1)
-
-        indx_class = []
-        scores_class = []
-        for class_confid in class_confidences:
-            scores = tf.expand_dims(pred_confidence[i], 0) * tf.expand_dims(class_confid, 0)
-            scores = tf.reshape(scores, [-1])
-            indx = tf.image.non_max_suppression(pred_bboxes, scores, 5)
-            indx_class.append(indx[tf.newaxis, :])
-            scores_class.append(scores[tf.newaxis, :])
-
-        indx_class = tf.concat(indx_class, axis=0)
-        #indx_class: the index after non-maximun suppression, [20, 5]
-
-        scores_class = tf.concat(scores_class, axis=0)
-        scores_class = scores_class.numpy()
-        #scores_class: confidence * class_confidence, [20, 98]
-
-        mask = np.zeros_like(scores_class)
-        for k in range(20):
-            for j in range(5):
-                mask[k, indx_class[k, j]] = 1
-        scores_class[scores_class < 0.2] = 0
-        scores_class *= mask
-        max_score = np.max(scores_class, axis=0)
-        indx_bboxes = np.arange(0, 98)
-        indx_bboxes = indx_bboxes[max_score > 0]
-        class_indx = np.argmax(scores_class, axis=0)
-        bbox_indx = class_indx[max_score > 0]
-        pred_bboxes_np = pred_bboxes.numpy()
-        boxes = pred_bboxes_np[indx_bboxes]
-
-        for k in range(boxes.shape[0]):
-            try:
-                img = draw_bbox(images[i].numpy() * 255, np.int32(boxes[k]), OBJECT_NAMES[bbox_indx[k]])
-                #Image.fromarray(np.uint8(img)).show()
-            except:
-                continue
-        Image.fromarray(np.uint8(img)).show()
+        np_result_[:, :, :, 0] = np_result_[:, :, :, 1]
+        np_result_[:, :, :, 1] = np_result_[:, :, :, 0]
+        np_result_[:, :, :, 2] = np_result_[:, :, :, 3]
+        np_result_[:, :, :, 3] = np_result_[:, :, :, 2]
+        box_data = np_result_
 
 
-        #classConditionalProbability = output[i]
-        #classConditionalProbability = classConditionalProbability[:, :, :20].numpy()
-        #objectProbability = output[i]
-        #objectProbability = objectProbability[:, :, 20:22].numpy()
-        #objectClassProbability = np.zeros([7, 7, 2, 20])
 
-        #for k in range(2):
-        #    for m in range(20):
-        #        objectClassProbability[:, :, k, m] = np.multiply(
-        #            objectProbability[:, :, k],
-        #            classConditionalProbability[:, :, m])
-        ##objectClassProbability = np.einsum(
-        ##     '...i, ...j',
-        ##     objectProbability,
-        ##     classConditionalProbability,
-        ##     out=objectClassProbability)
 
-        ## get max index in classes
-        #id_maxProbClasses = np.argmax(objectClassProbability, axis=3)
-        ## get max values in classes
-        #val_maxProbClasses = np.max(objectClassProbability, axis=3)
-        ## get 
-        #threshold_class_idx = np.where(val_maxProbClasses>=0.2)
 
-        ## classes
-        #id_thresholdedClasses = id_maxProbClasses[threshold_class_idx]
-        ## classes prob
-        #val_thresholdedClasses = val_maxProbClasses[threshold_class_idx]
 
-        #thres_box = box_data[threshold_class_idx[0], 
-        #                     threshold_class_idx[1],
-        #                     threshold_class_idx[2]]
 
-        ## class가 높은 확률을 가진것부터 오름차순으로 정렬
-        #sortOrder = np.argsort(val_thresholdedClasses)[::-1]    # 오름차순(큰수 -> 작은수)
-        #val_thresholdedClasses = val_thresholdedClasses[sortOrder]
 
-        #thres_box = thres_box[sortOrder]
-        #id_thresholdedClasses = id_thresholdedClasses[sortOrder]
 
-        ## NMS
-        #for box1 in range(len(val_thresholdedClasses)):
-        #    if val_thresholdedClasses[box1] == 0.:
-        #        continue
-        #    for box2 in range(box1 + 1, len(val_thresholdedClasses)):
-        #        if test_iou(thres_box[box1], thres_box[box2]) > 0.5:
-        #            val_thresholdedClasses[box2] = 0.
-        ## box
-        #non_suppresed_idx = np.where(val_thresholdedClasses > 0)
-        #val_thresholdedClasses = val_thresholdedClasses[non_suppresed_idx]
-        #thres_box = thres_box[non_suppresed_idx]
-        #id_thresholdedClasses = id_thresholdedClasses[non_suppresed_idx]
 
-        #image = tf.image.resize(images[i], [original_height_, original_width_])
-        #image = image.numpy()
 
-        #B = np.expand_dims(image[:, :, 2], 2)
-        #G = np.expand_dims(image[:, :, 1], 2)
-        #R = np.expand_dims(image[:, :, 0], 2)
 
-        #image = np.concatenate([B,G,R], 2)
 
-        #for h in range(len(id_thresholdedClasses)):
+        # 1. 각 바운딩 박스와 그 바운딩 박스의 중심좌료가 있는 그리데셀의 스코어 값들을 각각 곱함
+        pred_confidence_ = pred_confidence[i]
+        pred_confidence_ = tf.expand_dims(pred_confidence_, 0)  # [1, 7, 7, 2]
+        pred_class_ = pred_class[i]
+        pred_class_ = tf.expand_dims(pred_class_, 0)    # [1, 7, 7, 20]
+        pred_class_ = tf.split(pred_class_, num_or_size_splits=20, axis=-1)
 
-        #    X = int(thres_box[h][0])
-        #    Y = int(thres_box[h][1])
-        #    W = int(thres_box[h][2])
-        #    H = int(thres_box[h][3])
-        #    W = W // 2
-        #    H = H // 2
+        each_class_score_buf = []
+        for pred_cla in pred_class_:
+            each_class_score = pred_confidence_ * pred_cla  # [1, 7, 7, 2]
+            each_class_score = each_class_score.numpy() # [1, 7, 7, 2]
 
-        #    xmin, xmax, ymin, ymax = 0, 0, 0, 0
-        #    xmin = 3 if not max(X - W, 0) else (X - W)
-        #    xmax = original_width_ - 3 if not min(X + W - original_width_, 0) \
-        #                            else (X + W)
-        #    ymin = 1 if not max(Y - H, 0) else (Y - H)
-        #    ymax = original_height_ - 3 if not min(Y + H - original_height_, 0) \
-        #                            else (Y + H)
+            # 2. threshold 값으로 0.2를 지정, 해당 0.2 값보다 작은 결과는 다 0으로 만듬
+            for k in range(FLAGS.output_size):
+                for m in range(FLAGS.output_size):
+                    for b in range(2):
+                        if each_class_score[0, k, m, b] < FLAGS.mini_threshold:
+                            each_class_score[0, k, m, b] = 0.
+                        else:
+                            each_class_score[0, k, m, b] = each_class_score[0, k, m, b]
 
-        #    class_str = OBJECT_NAMES[id_thresholdedClasses[h]]
-        #    color = tuple([(j * (1+OBJECT_NAMES.index(class_str)) % 255) for j in seed])
+            each_class_score_buf.append(each_class_score)
+        each_class_score_buf = tf.convert_to_tensor(each_class_score_buf)   # [20, 1, 7, 7, 2]
+        each_class_score_buf = tf.transpose(each_class_score_buf, [1,2,3,4,0])  # [1, 7, 7, 2, 20]
 
-        #    cv2.rectangle(image, (xmin, ymin), (xmax, ymax), 2)
+        each_class_score_NPlist = each_class_score_buf.numpy()
 
-        #    if ymin <= 20:
-        #        cv2.rectangle(
-        #            image, (xmin, ymin), (xmax, ymin + 20), color, -1)
-        #        cv2.putText(
-        #            image, str(id_thresholdedClasses[h]) + ': %.2f' % val_thresholdedClasses[h],
-        #            (xmin+5, ymin+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        #            (0, 0, 0), 2)
-        #    else:
-        #        cv2.rectangle(image, (xmin, ymin), (xmax, ymin-20), color, -1)
-        #        cv2.putText(
-        #            image, str(id_thresholdedClasses[h]) + ': %.2f' % val_thresholdedClasses[h],
-        #            (xmin+5, ymin-8), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        #            (0, 0, 0), 2)
+        # 3. 각 박스에 있는값들 내림차순으로 정렬
+        boxidx_class = []
+        idx_class = []
+        val_class = []
+        for c in range(20):
+            sort_append = []
+            box = []
+            for k in range(FLAGS.output_size):
+                for m in range(FLAGS.output_size):
+                    for b in range(2):
+                        sort_append.append(each_class_score_buf[0, k, m, b, c].numpy())
+                        box.append(box_data[k, m, b, :])
 
-    return img
+            sort_val = np.sort(sort_append)[::-1]   # 내림차순 정렬 (value)   [98]
+            sort_idx = np.argsort(sort_append)[::-1]    # 내림차순 정렬 (index) [98]
+            sort_box = []
+            for ii in range(len(sort_append)):
+                sort_box.append(box[sort_idx[ii]])
+            sort_box = np.array(sort_box)   # 내림차순 정렬 (box) [98, 4]
+
+            # NMS
+            selected_indices = tf.image.non_max_suppression(sort_box, sort_val, 3)
+            selected_boxes = tf.gather(sort_box, selected_indices)
+
+            boxidx_class.append(selected_boxes[tf.newaxis, :])
+            val_class.append(sort_val[tf.newaxis, :])
+            idx_class.append(sort_idx[tf.newaxis, :])
+
+        boxidx_class = tf.concat(boxidx_class, 0)   # [3]
+        val_class = tf.concat(val_class, 0)     # []
+        idx_class = tf.concat(idx_class, 0)
+
+
+
+            # 박스 위치도 같이 움직여야 하는것 아닌가?
+
+
+
+
+    return image
 
 def cal_loss(model, images, labels):
     # https://github.com/lovish1234/YOLOv1/blob/master/yolo.py
-    """
-    0-19 CLass prediction
-    20-21 Confidence that objects exist in bbox1 or bbox2 of grid
-    22-29 Coordinates for bbo1, followed by those of bbox2
-    Real
-    0-19 Class prediction (One-Hot Encoded)
-    20-23 Ground truth coordinates for that box
-    24-72 Cell has an object/no object (Only one can be is 1)
-    """
+
     with tf.GradientTape(persistent=True) as tape:
         logits = model(images, training=True)
-        pred_class = (logits[:,:,:,:20])
-        predictedObjectConfidence = logits[:, :, :, 20:22]
-        predictedBoxes = logits[:, :, :, 22:]
-        predictedFirstBoxes = predictedBoxes[:, :, :, :4]
-        predictedSecondBoxes = predictedBoxes[:, :, :, 4:]
+        pred_class = tf.reshape(logits[:, 0:980], [-1,7,7,20])
+        predictedObjectConfidence = tf.reshape(logits[:, 980:1078], [-1, 7, 7, 2])
+        predictedBoxes = tf.reshape(logits[:, 1078:], [-1, 7, 7, 2, 4])
+        predictedFirstBoxes = predictedBoxes[:, :, :, 0:1, :]
+        predictedFirstBoxes = tf.squeeze(predictedFirstBoxes, 3)
+        predictedSecondBoxes = predictedBoxes[:, :, :, 1:2, :]
+        predictedSecondBoxes = tf.squeeze(predictedSecondBoxes, 3)
 
-        groundTruthClasses = labels[:, :, :, :20]
-        groundTruthBoxes = labels[:, :, :, 20:24]
-        groundTruthGrid = labels[:, :, :, 24:]
+        groundTruthClasses = labels[:, :, :, 5:]
+        groundTruthBoxes = labels[:, :, :, 0:4]
+        groundTruthGrid = labels[:, :, :, 4:5]
 
         firstBox_loss = tf.reduce_sum(tf.square(predictedFirstBoxes - groundTruthBoxes), 3)
         secondBox_loss = tf.reduce_sum(tf.square(predictedSecondBoxes - groundTruthBoxes), 3)
@@ -455,6 +357,8 @@ def main():
 
         if count % 100 == 0:
             img = save_fig(model, image, original_height, original_width)
+            cv2.imshow("ss", img)
+            cv2.waitKey(0)
 
         count += 1
 
